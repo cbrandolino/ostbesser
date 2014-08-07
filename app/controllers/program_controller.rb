f24aca28ab1fb46a5f84902a49424a5e51a83dea
Claudio Brandolino <cbrandolino@gmail.com>
2014-08-07 17:08:33 +0200

    Implement basic styling for cards

--- app/controllers/program_controller.rb
+++ app/controllers/program_controller.rb
@@ -1,5 +1,39 @@
 class ProgramController < ApplicationController
   def index
-
+    @dates = [
+      {
+        date: Date.new(2014, 10, 10),
+        time_start: DateTime.new(2014, 10, 10, 23, 59, 00, '+2'),
+        time_end: DateTime.new(2014, 10, 12, 12, 00, 00, '+2'),
+        name: 'Klubnacht',
+        events: [
+          location: 'Panorama Bar',
+          sets: [
+            {
+              dj: "the black madonna",
+              time_start: DateTime.new(2014, 10, 10, 23, 59, 00, '+2'),
+              time_start: DateTime.new(2014, 10, 11, 2, 00, 00, '+2')
+            }
+          ]
+        ]
+      },
+      {
+        date: Date.new(2014, 10, 10),
+        time_start: DateTime.new(2014, 10, 10, 23, 59, 00, '+2'),
+        time_end: DateTime.new(2014, 10, 12, 12, 00, 00, '+2'),
+        name: 'Klubnacht',
+        events: [
+          location: 'Panorama Bar',
+          sets: [
+            {
+              dj: "the black madonna",
+              time_start: DateTime.new(2014, 10, 10, 23, 59, 00, '+2'),
+              time_start: DateTime.new(2014, 10, 11, 2, 00, 00, '+2')
+            }
+          ]
+        ]
+      }
+    ]
+    @dates.map! { |el| OpenStruct.new el } 
   end
 end
